05f962f8730c4f51ddeb851749a457c1f1094733
Lindsay-07 <linaquino07@gmail.com>
2025-01-22 18:35:57 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Aquino
+{
+	class activity{
+		static void Main (string[] args)
+		{
+			string [] weeks = new[] {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",};
+			
+			for (int i = 0; i < weeks.Length; i++);
+			console.writeLine(weeks[i].ToUpper());
+		}
+	}
+}
